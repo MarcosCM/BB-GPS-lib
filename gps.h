ed8cc2025d150542994fd7da62984372023a5044
Marcos Canales <marketes94@gmail.com>
2016-06-05 13:55:20 +0200

    fixed headers

--- gps.h
+++ gps.h
@@ -32,6 +32,13 @@ int gps_read(char *buf);
  */
 int gps_write(const char *buf);
 /**
+ *	@brief Changes the device terminal baud rate.
+ *
+ *	@param buf Baud rate: 4800, 9600, 19200, 38400, 57600 or 115200
+ *	@return	Operation status: negative if error, otherwise success.
+ */
+int gps_change_baudrate(const char *str_baudrate);
+/**
  *	@brief Lib exit.
  *
  *	@return	Operation status: negative if error, otherwise success.
